64e35765e4e68d8eda7a69f6fc853a230fcbcea4
elenavonder <evonder13@gmail.com>
2020-12-22 12:49:23 -0500

    * .gitignore: initial commit
    * Program.cs:
    * TestingSolution.sln:
    * TestingProject.csproj:

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
